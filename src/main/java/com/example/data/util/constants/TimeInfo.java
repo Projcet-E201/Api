9c57ae6816ff9cf6028677b76321b0c44508a6b3
goeom77 <7eom14@gmail.com>
2023-05-14 11:04:19 +0900

    Feat 시간 부여 변경(machine)

--- src/main/java/com/example/data/util/constants/TimeInfo.java
+++ src/main/java/com/example/data/util/constants/TimeInfo.java
@@ -2,29 +2,29 @@ package com.example.data.util.constants;
 
 public class TimeInfo {
     //SENSOR
-    public static final String MACHINE_SENSOR_VELOCITY_START = "10m"; //"5m";
-    public static final String MACHINE_SENSOR_LOAD_START ="10m"; //"5m";
+    public static final String MACHINE_SENSOR_VELOCITY_START = "5m";
+    public static final String MACHINE_SENSOR_LOAD_START = "5m";
     public static final String MACHINE_SENSOR_ABRASION_START = "2d";
-    public static final String MACHINE_SENSOR_WATER_START = "2m";// "1m";
-    public static final String MACHINE_SENSOR_AIR_OUT_MPA_START = "20m"; //"10m";
-    public static final String MACHINE_SENSOR_AIR_OUT_KPA_START = "1h"; //"30m";
-    public static final String MACHINE_SENSOR_VACUUM_START = "10m"; //"5m";
-    public static final String MACHINE_SENSOR_MOTOR_START = "10m"; //"5m";
-    public static final String MACHINE_SENSOR_AIR_IN_KPA_START = "10m"; //"5m";
+    public static final String MACHINE_SENSOR_WATER_START = "1m";
+    public static final String MACHINE_SENSOR_AIR_OUT_MPA_START = "10m";
+    public static final String MACHINE_SENSOR_AIR_OUT_KPA_START = "30m";
+    public static final String MACHINE_SENSOR_VACUUM_START = "5m";
+    public static final String MACHINE_SENSOR_MOTOR_START = "5m";
+    public static final String MACHINE_SENSOR_AIR_IN_KPA_START = "5m";
 
     // STATE
     public static final String MACHINE_STATE_START = "2h"; // 20m
 
     // MACHINE
-    public static final String MACHINE_MOTOR_START = "4m"; //"2m";
-    public static final String MACHINE_AIR_IN_KPA_START = "1m"; //"24s";
-    public static final String MACHINE_AIR_OUT_KPA_START = "20m"; //"10m";
-    public static final String MACHINE_AIR_OUT_MPA_START = "10m"; //"4m";
-    public static final String MACHINE_VACUUM_START = "10m"; //"4m";
-    public static final String MACHINE_WATER_START = "2m"; //"40s";
+    public static final String MACHINE_MOTOR_START = "10m";
+    public static final String MACHINE_AIR_IN_KPA_START = "2m";
+    public static final String MACHINE_AIR_OUT_KPA_START = "20m";
+    public static final String MACHINE_AIR_OUT_MPA_START = "10m";
+    public static final String MACHINE_VACUUM_START = "10m";
+    public static final String MACHINE_WATER_START = "2m";
     public static final String MACHINE_ABRASION_START = "2d";
-    public static final String MACHINE_LOAD_START = "1h"; //"24m";
-    public static final String MACHINE_VELOCITY_START = "1h"; // "24m";
+    public static final String MACHINE_LOAD_START = "1h";
+    public static final String MACHINE_VELOCITY_START = "1h";
 
     // MAIN
     public static final String MAIN_MACHINE_START = "20m";// "2m";
